378df2b67adf4eab9374650e52310f02c3e13237
gerardo.cipriano <gerardo.cipriano@injenia.it>
2023-12-15 23:08:55 +0100

    Add NewVisitor feature and route

---
+++ src/Web/Features/NewVisitor/NewVisitor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Features.NewVisitor
+{
+    public class NewVisitor : Controller
+    {
+        [HttpGet("/newvisitor")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
